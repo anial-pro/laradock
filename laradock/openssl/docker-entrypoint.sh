4e60dab25720299b024001d6ce7d18d953f595cb
Artur <anialpro@gmail.com>
2019-10-23 12:58:57 +0400

    delete unnecessary files

--- laradock/openssl/docker-entrypoint.sh
+++ laradock/openssl/docker-entrypoint.sh
@@ -4,8 +4,9 @@ for SITE_NAME in ${OPENSSL_SITE_DOMAINS[@]}; do
 
     if [[ ! ( -f "/ssl/${SITE_NAME}.key" && -f "/ssl/${SITE_NAME}.crt" && -f "/ssl/${SITE_NAME}.csr" ) ]]; then
         openssl genrsa -out "/ssl/${SITE_NAME}.key" 2048 \
-        && openssl req -new -key "/ssl/${SITE_NAME}.key" -out "/ssl/${SITE_NAME}.csr" -subj "/CN=${SITE_NAME}/O=${SITE_NAME}/C=UK" \
-        && openssl x509 -req -days 365 -in "/ssl/${SITE_NAME}.csr" -signkey "/ssl/${SITE_NAME}.key" -out "/ssl/${SITE_NAME}.crt"
+        && openssl req -new -key "/ssl/${SITE_NAME}.key" -out "/ssl/${SITE_NAME}.csr" -subj "/CN=*.${SITE_NAME}/O=${SITE_NAME}/C=UK" \
+        && openssl x509 -req -days 365 -in "/ssl/${SITE_NAME}.csr" -signkey "/ssl/${SITE_NAME}.key" -out "/ssl/${SITE_NAME}.crt" \
+        && echo "Create certs for ${SITE_NAME}"
     else
         echo "Nothing to do"
     fi
